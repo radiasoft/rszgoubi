fb726913c63198dc23ebe3ee8d6f9751b853c703
Pepe Barbe <pepe@radiasoft.net>
2017-03-02 13:44:52 -0600

    Use a docker container instead of Vagrant

---
+++ container-conf/build.sh
@@ -0,0 +1,18 @@
+build_image_base=fedora:latest
+src_dir=/src
+zgoubi_dir=/src/zgoubi
+
+build_zgoubi() {
+    svn co https://svn.code.sf.net/p/zgoubi/code/trunk "$zgoubi_dir"
+    cd "$zgoubi_dir"
+    #make -j -f Makefile_zgoubi_gfortran 
+}
+
+build_as_run_user() {
+    (build_zgoubi)
+}
+
+build_as_root() {
+    mkdir "$src_dir"
+    chown -R vagrant: "$src_dir"
+}
