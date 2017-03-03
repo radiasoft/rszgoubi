1bd22e64aef3533704dd83ceedf98e023e4bac06
Pepe Barbe <pepe@radiasoft.net>
2017-03-02 18:50:42 -0600

    Build zgoubi in parallel

--- container-conf/build.sh
+++ container-conf/build.sh
@@ -5,7 +5,7 @@ zgoubi_dir=/src/zgoubi
 build_zgoubi() {
     svn co https://svn.code.sf.net/p/zgoubi/code/trunk "$zgoubi_dir"
     cd "$zgoubi_dir"
-    make -f Makefile_zgoubi_gfortran 
+    make -j 4 -f Makefile_zgoubi_gfortran 
 }
 
 build_as_run_user() {
