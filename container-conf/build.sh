e1604bd0af1cf38968778bf38a645017b7961793
Pepe Barbe <pepe@radiasoft.net>
2017-03-03 23:10:13 -0600

    Only build szgoubi

--- container-conf/build.sh
+++ container-conf/build.sh
@@ -1,18 +1,13 @@
 build_image_base=fedora:latest
-src_dir=/src
-zgoubi_dir=/src/zgoubi
+zgoubi_dir="$HOME/src/zgoubi"
 
 build_zgoubi() {
-    svn co https://svn.code.sf.net/p/zgoubi/code/trunk "$zgoubi_dir"
+    git clone -b rszgoubi --depth 1 https://github.com/radiasoft/zgoubi "$zgoubi_dir"
     cd "$zgoubi_dir"
-    make FFLAGS="-O4 -Wall -fno-automatic -pedantic -cpp -gdwarf-2" -j 8 -f Makefile_zgoubi_gfortran 
+    make FFLAGS="-O4 -Wall -fno-automatic -pedantic -cpp -gdwarf-2" zgoubi
+    ln -s "$zgoubi_dir/zgoubi/zgoubi" $HOME
 }
 
 build_as_run_user() {
     (build_zgoubi)
-}
-
-build_as_root() {
-    mkdir "$src_dir"
-    chown -R vagrant: "$src_dir"
 }
