47aea9c11f0895449d407fc9c7deaf3f8ca1c85a
Pepe Barbe <pepe@radiasoft.net>
2017-03-06 12:52:17 -0600

    Add zgoubi to the PATH

--- container-conf/build.sh
+++ container-conf/build.sh
@@ -5,7 +5,7 @@ build_zgoubi() {
     git clone -b rszgoubi --depth 1 https://github.com/radiasoft/zgoubi "$zgoubi_dir"
     cd "$zgoubi_dir"
     make FFLAGS="-O4 -Wall -fno-automatic -pedantic -cpp -gdwarf-2" zgoubi
-    ln -s "$zgoubi_dir/zgoubi/zgoubi" $HOME
+    ln -s "$zgoubi_dir/zgoubi/zgoubi" "$HOME/bin"
 }
 
 build_as_run_user() {
