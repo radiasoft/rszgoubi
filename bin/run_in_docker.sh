11dbd14bd7cc4582abe000f937f238c250094250
Pepe Barbe <pepe@radiasoft.net>
2017-03-10 20:40:34 -0600

    Start container detached so it can run in the background

--- bin/run_in_docker.sh
+++ bin/run_in_docker.sh
@@ -5,4 +5,7 @@ set -e -u -o pipefail
 zgoubi_results=$1
 shift
 
-docker run -u vagrant -v "$zgoubi_results:/zgoubi_results" radiasoft/rszgoubi:latest /rszgoubi-run.sh "$@"
+zgoubi_run_cmd="$@"
+
+docker logs -f $(docker run -d --rm -u vagrant -v "$zgoubi_results:/zgoubi_results" \
+    radiasoft/rszgoubi:latest /rszgoubi-run.sh "$zgoubi_run_cmd")
